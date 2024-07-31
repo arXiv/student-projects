55ad607b16a9f48197f5fe2e7bccc7daed986b1f
btanowitz <btanowitz@arxiv.org>
2024-07-31 15:35:53 -0400

    added check_update function for data

--- public-usage-project/run.py
+++ public-usage-project/run.py
@@ -1,5 +1,30 @@
 # An instance of the app.
-from app import create_app
+from app import create_app, download_data
+from datetime import datetime, timezone, timedelta
+import time
+
+import threading
+
+last_run_month = datetime(2024,6,1)
+last_run_hour = datetime(2024, 7,31,0)
+
+def check_update():
+    while(True):
+        curr_time = datetime.now(timezone(timedelta(hours=-4)))
+
+        if (curr_time.month > last_run_month.month):
+            last_run_month = curr_time
+            download_data.monthly_data()
+
+        if (curr_time - last_run_month):
+            last_run_hour = curr_time
+            download_data.daily_data(last_run_hour.strftime("%y%M%d"))
+        time.sleep(3600)
 
 if __name__ == '__main__':
+
+    t1 = threading.Thread(target=check_update())
+    t1.start()
+
     create_app().run(host='0.0.0.0', debug=True)
+
