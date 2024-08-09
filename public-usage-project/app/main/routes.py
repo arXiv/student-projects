d0a775109ec8c1a9d2d0cf60c7bfd47042e0db2d
Wong-Benjamin <benjaminwong02@gmail.com>
2024-08-09 16:26:14 -0400

    Api.py overhaul to SQLAlchemy,  demo geograph page, and general housekeeping.
    Api.py now uses sqlAlchemy, and uses an ORM model based on the one sheet we currently have (hourly download data.) There are currently 2 functioning endpoints:
    1. Get hourly primary count; global downloads per hour
    2. Get primary count by country: Overall downloads per country
    
    A new downloads by country geomap is in place as an example of a functioning plot using the API and Plotly.
    
    Now that we are using PlotlyJS to render our plots from now on, I have taken the opportunity to do some housekeeping:
    - download_data.py is deleted now that Erin is handling the database writing
    - old templates using Bokeh/Google Charts are deleted, and so are their corresponding routes
    - With the new database format, obsolete API endpoints are deleted.
    - local testing examples deleted now that they do not follow the new format.
    
    Part of ongoing efforts towards [ARXIVCE-2415].

--- public-usage-project/app/main/routes.py
+++ public-usage-project/app/main/routes.py
@@ -8,22 +8,16 @@ def index():
 
 @main.route('/hourly_usage', methods=['GET'])
 def render_hourly_usage():
-    return render_template('hourly_usage.html')
+    return render_template('hourly_usage.html') 
 
-
-@main.route('/monthly_downloads', methods=['GET'])
+""" @main.route('/monthly_downloads', methods=['GET'])
 def render_monthly_downloads():
-    return render_template('monthly_downloads.html')
+    return render_template('monthly_downloads.html') """
 
-
-@main.route('/monthly_submissions', methods=['GET'])
+""" @main.route('/monthly_submissions', methods=['GET'])
 def render_monthly_submissions():
-    return render_template('monthly_submissions.html')
+    return render_template('monthly_submissions.html') """
 
-@main.route('/old_monthly_submissions', methods=['GET'])
-def render_old_monthly_submissions():
-    return render_template('old_monthly_submissions.html')
-
-@main.route('/alt_monthly_downloads', methods=['GET'])
-def render_alt_monthly_downloads():
-    return render_template('alt_monthly_downloads.html')
+@main.route('/downloads_by_country', methods=['GET'])
+def render_downloads_by_country():
+    return render_template('downloads_by_country.html')
