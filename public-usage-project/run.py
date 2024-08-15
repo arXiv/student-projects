f1abfc2aa290e8b332406cdc19664928c74aa475
Wong-Benjamin <benjaminwong02@gmail.com>
2024-08-15 12:45:41 -0400

    Minor additions to landing page
    Added a couple of more lines of text to make navigation more apparent.

--- public-usage-project/run.py
+++ public-usage-project/run.py
@@ -3,5 +3,5 @@ from app import create_app
 import os
 
 if __name__ == '__main__':
-    create_app().run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=True)
-    #create_app().run(port=int(os.environ.get("PORT", 8080)), debug=True)
+    #create_app().run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=True)
+    create_app().run(port=int(os.environ.get("PORT", 8080)), debug=True)
