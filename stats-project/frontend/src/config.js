5f32144c8fe9982b1242c2c60ca02691535d757e
Wong-Benjamin <benjaminwong02@gmail.com>
2025-05-30 11:46:52 -0400

    Dockerfile update and config.js
    Dockerfile now properly functions as meant to and config.js now properly looks for env variables (Had to specify REACT_APP_).

--- stats-project/frontend/src/config.js
+++ stats-project/frontend/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 // This file contains configuration settings for the frontend application, fetching our API base URL from environment variables or defaulting to a local server.
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
