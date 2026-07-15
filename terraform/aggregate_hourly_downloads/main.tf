1b4fcb5bfcd3c1abdb3de289c4b9854fc51ce701
Carly Jones <55803050+carly-jones@users.noreply.github.com>
2026-07-15 14:29:58 -0400

    [QA-257] Update alerting for hourly downloads cron (#208)
    * update logging, update alert to only trigger on NoRetryError
    
    * add oom alert

--- terraform/aggregate_hourly_downloads/main.tf
+++ terraform/aggregate_hourly_downloads/main.tf
@@ -157,14 +157,14 @@ resource "google_cloud_scheduler_job" "invoke_cloud_function" {
 ### alerting ###
 
 resource "google_monitoring_alert_policy" "cloud_run_error_alert" {
-  display_name = "${google_cloudfunctions2_function.function.name} logging errors"
+  display_name = "${google_cloudfunctions2_function.function.name} NoRetryError"
   combiner     = "OR"
   severity     = "ERROR"
 
   conditions {
-    display_name = "Cloud Run error log"
+    display_name = "Cloud Run NoRetryError log"
     condition_matched_log {
-      filter = "resource.type=\"cloud_run_revision\" AND severity=(\"ERROR\" OR \"CRITICAL\" OR \"ALERT\" OR \"EMERGENCY\") AND resource.labels.service_name=\"${google_cloudfunctions2_function.function.name}\""
+      filter = "resource.type=\"cloud_run_revision\" AND resource.labels.service_name=\"${google_cloudfunctions2_function.function.name}\" AND jsonPayload.message=~\"A NoRetry exception has been raised\""
     }
   }
 
@@ -179,7 +179,35 @@ resource "google_monitoring_alert_policy" "cloud_run_error_alert" {
   ]
 
   documentation {
-    content   = "Cloud Run service ${google_cloudfunctions2_function.function.name} has reported an error - see logs."
+    content   = "Cloud Run service ${google_cloudfunctions2_function.function.name} raised a NoRetryError and will not retry - see logs, fix the problem, and manually run the function to patch data as needed."
+    mime_type = "text/markdown"
+  }
+}
+
+resource "google_monitoring_alert_policy" "cloud_run_oom_alert" {
+  display_name = "${google_cloudfunctions2_function.function.name} OOM"
+  combiner     = "OR"
+  severity     = "ERROR"
+
+  conditions {
+    display_name = "Cloud Run OOM log"
+    condition_matched_log {
+      filter = "resource.type=\"cloud_run_revision\" AND resource.labels.service_name=\"${google_cloudfunctions2_function.function.name}\" AND severity=ERROR AND textPayload=~\"Memory limit of.*exceeded\""
+    }
+  }
+
+  alert_strategy {
+    notification_rate_limit {
+      period = "300s" # limit notifications to every 5 minutes
+    }
+  }
+
+  notification_channels = [
+    "projects/${var.gcp_project_id}/notificationChannels/${var.slack_channel_id}"
+  ]
+
+  documentation {
+    content   = "Cloud Run service ${google_cloudfunctions2_function.function.name} was killed for exceeding its memory limit (OOM) - see logs to determine whether the container's available_memory needs to be increased or whether the function is leaking/overusing memory."
     mime_type = "text/markdown"
   }
 }
